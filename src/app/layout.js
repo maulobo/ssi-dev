b8b6bca15229cec728b7e01217467cb723aca08a
Mauro Lobo <maurolobo.ml@gmail.com>
2024-03-13 11:26:05 -0300

    Update layout.js with google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <head>
+        {/* Google Analytics script */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
+      </head>
       <body>
         <Menu />
         {children}
