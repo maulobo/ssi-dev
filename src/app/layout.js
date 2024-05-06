4e5b5a2f0aec4b24be9abcc7705793c9f72ca1d2
Mauro <maurolobo.ml@gmail.com>
2024-05-06 19:31:49 -0300

    agrego title

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="description"
+          content="Somos una compañía de servicios dirigidos a satisfacer las necesidades de la actividad industrial y de empresas e instituciones de gran volumen en la región Patagónica."
+        />
+        <title>Soto Servicios Industriales</title>
         {/* Google Analytics script */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -28,7 +33,10 @@ export default function RootLayout({ children }) {
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
           `}
         </Script>
-          <meta name="google-site-verification" content="_Y6_3j9GnIFgxhcEC4oKpEVtQONgPZ_2LS3TPoDF_HQ" />
+        <meta
+          name="google-site-verification"
+          content="_Y6_3j9GnIFgxhcEC4oKpEVtQONgPZ_2LS3TPoDF_HQ"
+        />
       </head>
       <body>
         <Menu />
