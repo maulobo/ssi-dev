4d37a110f4cb41f5fbc65f60cf1dd817e8064bd9
Mauro Lobo <maurolobo.ml@gmail.com>
2025-05-21 16:21:32 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -41,66 +41,6 @@ export default function Home() {
             necesidades de la actividad industrial y de empresas e instituciones
             de gran volumen en la región Patagónica.
           </p>
-                    <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "Organization",
-            "name": "Soto Servicios Industriales",
-            "url": "https://sotosisrl.com",
-            "logo": "https://www.tuempresa.com/logo.png",
-            "description": "Especialistas en trailers armados, catering para empresas, equipamiento petrolero y módulos habitacionales.",
-            "telephone": "2994870406",
-            "email": "logistica@ssisrl.com.ar",
-            "address": {
-              "@type": "PostalAddress",
-              "streetAddress": "",
-              "addressLocality": "Neuquen",
-              "addressRegion": "Neuquen",
-              "postalCode": "8300",
-              "addressCountry": "Argentina"
-            },
-            "sameAs": [
-              "https://www.facebook.com/tuempresa",
-              "https://www.instagram.com/tuempresa",
-              "https://www.linkedin.com/company/tuempresa"
-            ],
-            "offers": [
-              {
-                "@type": "Offer",
-                "itemOffered": {
-                  "@type": "Product",
-                  "name": "Trailers Armados",
-                  "description": "Trailers personalizados para diversos usos comerciales y residenciales.",
-                  "image": "",
-                  "url": "https://www.tuempresa.com/trailers"
-                }
-              },
-              {
-                "@type": "Offer",
-                "itemOffered": {
-                  "@type": "Service",
-                  "name": "Servicios de Catering",
-                  "description": "Servicios de catering profesional para eventos corporativos y proyectos industriales.",
-                  "image": "",
-                  "url": "https://sotosisrl.com/catering"
-                }
-              },
-              {
-                "@type": "Offer",
-                "itemOffered": {
-                  "@type": "Trailers",
-                  "name": "Módulos Habitacionales",
-                  "description": "Módulos habitacionales para proyectos industriales y campamentos temporales.",
-                  "image": "",
-                  "url": "https://sotosisrl.com/trailers"
-                }
-              }
-            ]
-          })
-        }}
-      />
           <HomeHero />
           <Letters />
           <Empresa />
